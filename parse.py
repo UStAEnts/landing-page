e375074c2585055cdc293d4fddfb45670428582c
Ryan Delaney <vitineth@gmail.com>
2023-01-25 19:01:35 +0000

    Removing reliance on google fonts
    Turns out this hangs the page when you don't have them in cache. This was a mistake. This also removes main.css and main.js which don't exist

--- parse.py
+++ parse.py
@@ -136,12 +136,6 @@ PAGE_TEMPLATE = """
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
 
     <style>
         {css}
